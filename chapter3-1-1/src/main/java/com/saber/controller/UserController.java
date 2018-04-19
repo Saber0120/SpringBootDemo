1e674942d0ad1f9e007263d1018e13944d469d51
盛凯华 <kaihua.sheng@magustek.com>
2018-04-19 16:15:00 +0800

    chapter3-1-1：使用swagger2构建RESTful API

--- chapter3-1-1/src/main/java/com/saber/controller/UserController.java
+++ chapter3-1-1/src/main/java/com/saber/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.saber.controller;
 
 import com.saber.entity.User;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -15,6 +16,7 @@ public class UserController {
     static Map<Long, User> userMap = Collections.synchronizedMap(new HashMap<>());
 
     // 处理get请求
+    @ApiOperation(value = "获取用户列表", notes = "获取全部用户列表")
     @GetMapping
     public List<User> getUserList() {
         List<User> result = new ArrayList<>(userMap.values());
@@ -22,17 +24,20 @@ public class UserController {
     }
 
     // 处理post请求
+    @ApiOperation(value = "新增用户", notes = "新增用户，成功返回success")
     @PostMapping
     public String addUser(@ModelAttribute User user) {
         userMap.put(user.getId(), user);
         return "success";
     }
 
+    @ApiOperation(value = "获取用户详情", notes = "根据用户id获取用户详情")
     @GetMapping("/{id}")
     public User getUser(@PathVariable("id") Long id) {
         return userMap.get(id);
     }
 
+    @ApiOperation(value = "更新用户", notes = "更新指定用户id的用户信息")
     @PutMapping("/{id}")
     public String putUser(@PathVariable("id") Long id, @ModelAttribute User user) {
         User u = userMap.get(id);
@@ -42,6 +47,7 @@ public class UserController {
         return "success";
     }
 
+    @ApiOperation(value = "删除用户", notes = "根据用户id删除用户")
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
         userMap.remove(id);
